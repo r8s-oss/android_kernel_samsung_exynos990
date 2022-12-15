49fabfa9907aa12095292afae105acf2a675faab
dunno <dunno@dontca.re>
2022-12-15 16:41:40 +0100

    kernel: update changes from Samsung Open Source
    * Source package: SM-G780F_CIS_13_Opensource.zip

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-export PLATFORM_VERSION=11
-export ANDROID_MAJOR_VERSION=r
+export SEC_BUILD_CONF_VENDOR_BUILD_OS=13
+export PLATFORM_VERSION=13
+export ANDROID_MAJOR_VERSION=T
 export ARCH=arm64
 
-make exynos9830-y2sxxx_defconfig
+make exynos9830-r8slte_defconfig
 make -j16
 
